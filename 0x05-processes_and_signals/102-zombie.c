18cf35b6374024b4de1ad42ea0529ccb7178fed7
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-09-29 18:45:40 -0700

    smith made sure it's checked

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Creates an infinite loop
+ *
+ * Return: Always 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Creates 5 zombie processes
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("Fork failed");
+			return (1);
+		}
+
+		if (child_pid == 0)
+		{
+			/* Child process */
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+
+	infinite_while();
+
+	return (0);
+}
